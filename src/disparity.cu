bf5c5f69b9d85e04a719b4b539d2fceb290b81da
Nigel Dias <nigeldias2012@gmail.com>
2021-06-21 13:56:00 -0300

    adding stride, changing variables name and some identation

--- src/disparity.cu
+++ src/disparity.cu
@@ -18,30 +18,40 @@ __global__
 void compute_sad (const cv::cudev::PtrStepSz<uchar> im_l, const cv::cudev::PtrStepSz<uchar> im_r,
                 const int win_size, const int disp_range, cv::cudev::PtrStepSz<uchar> disp_map)
 {
-    const int x = blockDim.x * blockIdx.x + threadIdx.x;
-	const int y = blockDim.y * blockIdx.y + threadIdx.y;
+    const int idx = blockDim.x * blockIdx.x + threadIdx.x;
+	const int idy = blockDim.y * blockIdx.y + threadIdx.y;
     
+    const int stride_x  = blockDim.x * gridDim.x;
+    const int stride_y  = blockDim.y * gridDim.y;
+
     int min_sad     = INT_MAX;
     int disp_value  = 0.0;
 
-    if(y >= win_size/2 && y < im_l.rows - win_size/2)
+    for (int x = idx; x < im_l.cols; x+= stride_x)
     {
-        if(x >= win_size/2 && x < im_l.cols - win_size/2)
+        for (int y = idy; y < im_l.rows; y+= stride_y)
         {
-            for(int d = 0; d < disp_range; d++)
+            if(y >= win_size/2 && y < im_l.rows - win_size/2)
             {
-                if((x + d) >= (im_l.cols - win_size/2))
-                    break;
-                int sad_value = compute_sad(im_l, im_r, win_size, x, y, d);
-                if (sad_value < min_sad)
+                if(x >= win_size/2 && x < im_l.cols - win_size/2)
                 {
-                    min_sad     = sad_value;
-                    disp_value  = d;
-                }                    
+                    for(int d = 0; d < disp_range; d++)
+                    {
+                        if((x + d) >= (im_l.cols - win_size/2))
+                            break;
+                        int sad_value = compute_sad(im_l, im_r, win_size, x, y, d);
+                        if (sad_value < min_sad)
+                        {
+                            min_sad     = sad_value;
+                            disp_value  = d;
+                        }                    
+                    }
+                    disp_map.ptr(y)[x] = (uchar) disp_value;
+                }
             }
-            disp_map.ptr(y)[x] = (uchar) disp_value;
         }
     }
+    
     
 }
 
@@ -49,14 +59,19 @@ void compute_sad (const cv::cudev::PtrStepSz<uchar> im_l, const cv::cudev::PtrSt
 double compute_disparity_gpu (const cv::cuda::GpuMat &im_l, const cv::cuda::GpuMat &im_r,
     const int &win_size, const int &disp_range, cv::cuda::GpuMat &disp_map)
 {
-    const dim3 block(64, 2);
-	const dim3 grid(cv::cudev::divUp(disp_map.cols, block.x), cv::cudev::divUp(disp_map.rows, block.y));
+    const dim3 threadsPerBlock(32, 32);
+	const dim3 blocksPerGrid(cv::cudev::divUp(disp_map.cols, threadsPerBlock.x), 
+                        cv::cudev::divUp(disp_map.rows, threadsPerBlock.y));
     
     auto start = std::chrono::steady_clock::now();
-    compute_sad<<<grid, block>>>(im_l, im_r, win_size, disp_range, disp_map);
+    
+    compute_sad<<<blocksPerGrid, threadsPerBlock>>>(im_l, im_r, win_size, disp_range, disp_map);
+    
     CV_CUDEV_SAFE_CALL(cudaGetLastError());
 	CV_CUDEV_SAFE_CALL(cudaDeviceSynchronize());
+    
     auto end = std::chrono::steady_clock::now();
     auto time = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+    
     return time;
 }
