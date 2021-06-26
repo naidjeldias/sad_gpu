90421bf7750dc927d33cdd0ddbab18ca0cc02fbc
Nigel Dias <nigeldias2012@gmail.com>
2021-06-26 12:26:25 -0300

    using 1D vector instead of 2D matrix

--- src/disparity.cu
+++ src/disparity.cu
@@ -2,20 +2,21 @@
 #include <opencv2/cudev.hpp>
 #include <cuda_runtime.h>
 
-__device__ int compute_sad(const cv::cudev::PtrStepSz<uchar> im_l, const cv::cudev::PtrStepSz<uchar> im_r,
-            const int &win_size, const int x, const int y, const int d)
+__device__ int compute_sad(unsigned char *im_l, unsigned char *im_r,
+            const int &win_size, const int x, const int y, const int d, const int width)
 {
     int start = -(win_size )/2;
 	int stop  = win_size -1;
 	int sad_value = 0;
 	for(int i = start; i < stop; i++)
 		for(int j = start; j < stop; j++)
-			sad_value += abs((int)im_l.ptr(y + j)[x + i] - (int)im_r.ptr(y + j)[x - d + i]);
+            sad_value += abs((int)im_l[(y+j) * width + (x+i)] - (int)im_r[(y+j) * width + (x-d+i)]);
 	return sad_value;
 }
 __global__ 
-void compute_sad (const cv::cudev::PtrStepSz<uchar> im_l, const cv::cudev::PtrStepSz<uchar> im_r,
-                const int win_size, const int disp_range, cv::cudev::PtrStepSz<uchar> disp_map)
+void compute_sad (unsigned char *im_l, unsigned char *im_r,
+                const int win_size, const int disp_range, unsigned char *disp_map, const int width,
+    const int height)
 {
     const int idx = blockDim.x * blockIdx.x + threadIdx.x;
 	const int idy = blockDim.y * blockIdx.y + threadIdx.y;
@@ -26,26 +27,26 @@ void compute_sad (const cv::cudev::PtrStepSz<uchar> im_l, const cv::cudev::PtrSt
     int min_sad     = INT_MAX;
     int disp_value  = 0.0;
 
-    for (int x = idx; x < im_l.cols; x+= stride_x)
+    for (int x = idx; x < width; x+= stride_x)
     {
-        for (int y = idy; y < im_l.rows; y+= stride_y)
+        for (int y = idy; y < height; y+= stride_y)
         {
-            if(y >= win_size/2 && y < im_l.rows - win_size/2)
+            if(y >= win_size/2 && y < height - win_size/2)
             {
-                if(x >= win_size/2 && x < im_l.cols - win_size/2)
+                if(x >= win_size/2 && x < width - win_size/2)
                 {
                     for(int d = 0; d < disp_range; d++)
                     {
-                        if((x + d) >= (im_l.cols - win_size/2))
+                        if((x + d) >= (width - win_size/2))
                             break;
-                        int sad_value = compute_sad(im_l, im_r, win_size, x, y, d);
+                        int sad_value = compute_sad(im_l, im_r, win_size, x, y, d, width);
                         if (sad_value < min_sad)
                         {
                             min_sad     = sad_value;
                             disp_value  = d;
                         }                    
                     }
-                    disp_map.ptr(y)[x] = (uchar) disp_value;
+                    disp_map[y * width + x] = (uchar) disp_value;
                 }
             }
         }
@@ -61,25 +62,21 @@ double compute_disparity_gpu (const cv::Mat &im_left, const cv::Mat &im_right,
     int deviceId;
     cudaGetDevice(&deviceId);
     unsigned int frameByteSize = im_left.rows * im_left.cols;
-
+    
     //Input 
-    void *im_letf_ptr;
+    unsigned char *im_letf_ptr;
     cudaMallocManaged(&im_letf_ptr, frameByteSize);
     cudaMemcpy(im_letf_ptr, im_left.ptr(), frameByteSize, cudaMemcpyHostToDevice);
-    cv::cuda::GpuMat im_l   (im_left.size(), CV_8UC1, im_letf_ptr);
 
-    void *im_right_ptr;
+    unsigned char *im_right_ptr;
     cudaMallocManaged(&im_right_ptr, frameByteSize);
     cudaMemcpy(im_right_ptr, im_right.ptr(), frameByteSize, cudaMemcpyHostToDevice);
-    cv::cuda::GpuMat im_r   (im_right.size(), CV_8UC1, im_right_ptr);
 
     //Output
-    void *disp_map_ptr;	
+    unsigned char *disp_map_ptr;	
 	cudaMallocManaged(&disp_map_ptr, frameByteSize);
     cudaMemset(disp_map_ptr, 0, frameByteSize*sizeof(uchar));
-    cv::cuda::GpuMat  	d_disp_map_gpu 	(im_left.size(), CV_8UC1, disp_map_ptr);
     
-
     //Prefetching data
     cudaMemPrefetchAsync(im_letf_ptr, frameByteSize, deviceId);
     cudaMemPrefetchAsync(im_right_ptr, frameByteSize, deviceId);
@@ -91,7 +88,7 @@ double compute_disparity_gpu (const cv::Mat &im_left, const cv::Mat &im_right,
     
     auto start = std::chrono::steady_clock::now();
 
-    compute_sad<<<blocksPerGrid, threadsPerBlock>>>(im_l, im_r, win_size, disp_range, d_disp_map_gpu);
+    compute_sad<<<blocksPerGrid, threadsPerBlock>>>(im_letf_ptr, im_right_ptr, win_size, disp_range, disp_map_ptr, disp_map.cols, disp_map.rows);
     
     CV_CUDEV_SAFE_CALL(cudaGetLastError());
 	CV_CUDEV_SAFE_CALL(cudaDeviceSynchronize());
@@ -101,7 +98,7 @@ double compute_disparity_gpu (const cv::Mat &im_left, const cv::Mat &im_right,
 
     //Copy data from device to host
     cudaMemPrefetchAsync(disp_map_ptr, frameByteSize, cudaCpuDeviceId);
-    cudaMemcpy(disp_map.ptr(), d_disp_map_gpu.ptr(), frameByteSize, cudaMemcpyDeviceToHost);
+    cudaMemcpy(disp_map.ptr(), disp_map_ptr, frameByteSize, cudaMemcpyDeviceToHost);
 
     cudaFree(im_letf_ptr);
     cudaFree(im_right_ptr);
